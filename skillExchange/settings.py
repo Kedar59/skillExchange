2e876a9059cc402c5a25f3c03b91de7eb53f7fc3
Kedar59 <kedarshidhaye2003@gmail.com>
2023-09-26 17:34:15 +0530

    please fix error

--- skillExchange/settings.py
+++ skillExchange/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL='users.CustomUser'
-AUTHENTICATION_BACKENDS=['django.conrib.auth.backends.ModelBackends']
+AUTHENTICATION_BACKENDS=['django.contrib.auth.backends.ModelBackends']
 # Application definition
 
 INSTALLED_APPS = [
