d369c1a9a2c5c6f2ff4375fe4871c8d405109726
Kedar59 <kedarshidhaye2003@gmail.com>
2023-10-03 18:37:12 +0530

    firstname change works perfectly

--- skillExchange/settings.py
+++ skillExchange/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL='users.CustomUser'
-AUTHENTICATION_BACKENDS=['django.contrib.auth.backends.ModelBackends']
+AUTHENTICATION_BACKENDS=['django.contrib.auth.backends.ModelBackend']
 # Application definition
 
 INSTALLED_APPS = [
