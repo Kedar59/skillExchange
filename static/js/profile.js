cd6c650ba7e2a595c67bc39b86337263d769d8df
Kedar59 <kedarshidhaye2003@gmail.com>
2023-10-04 17:02:10 +0530

    trying to apply profile pic

--- static/js/profile.js
+++ static/js/profile.js
@@ -3,7 +3,55 @@ document.addEventListener('DOMContentLoaded', function () {
     const updateFirstNameForm = document.getElementById('update-first-name-form');
 
     updateFirstNameButton.addEventListener('click', function () {
+        console.log("I was clicked");
         updateFirstNameForm.style.display = 'block';
         updateFirstNameButton.style.display = 'none';
+    });
+
+    const updateLastNameButton = document.getElementById('update-last-name-button');
+    const updateLastNameForm = document.getElementById('update-last-name-form');
+
+    updateLastNameButton.addEventListener('click', function () {
+        updateLastNameForm.style.display = 'block';
+        updateLastNameButton.style.display = 'none';
+    });
+
+    const updateUserBioButton = document.getElementById('update-userBio-button');
+    const updateUserBioForm = document.getElementById('update-userBio-form');
+
+    updateUserBioButton.addEventListener('click', function () {
+        updateUserBioForm.style.display = 'block';
+        updateUserBioButton.style.display = 'none';
+    });
+
+    const setProfPicButton = document.getElementById('set-prof-pic-button');
+    const updateProfPicButton = document.getElementById('update-prof-pic-button');
+    const updateProfPicForm = document.getElementById('update-prof-pic-form');
+
+    updateProfPicButton.addEventListener('click', function () {
+        updateProfPicForm.style.display = 'block';
+        updateProfPicButton.style.display = 'none';
+    });
+
+    setProfPicButton.addEventListener('click', function () {
+        updateProfPicForm.style.display = 'block';
+        setProfPicButton.style.display = 'none';
+    });
+
+    document.getElementById('profilePicInput').addEventListener('change', function () {
+        const profilePicInput = document.getElementById('profilePicInput');
+        const fileSizeError = document.getElementById('fileSizeError');
+    
+        if (profilePicInput.files.length > 0) {
+            const fileSize = profilePicInput.files[0].size; // Size in bytes
+            const maxSize = 250 * 1024; // 250KB in bytes
+    
+            if (fileSize > maxSize) {
+                fileSizeError.style.display = 'block';
+                profilePicInput.value = ''; // Clear the input to prevent upload
+            } else {
+                fileSizeError.style.display = 'none';
+            }
+        }
     });
 });
--- users/models.py
+++ users/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email=models.EmailField(unique=True)
     userBio=models.CharField(max_length=255,blank=False)
+    prof_path=models.CharField(max_length=1024,null=True,blank=True)
+    has_prof_pic=models.BooleanField(default=False)
+    prof_extension=models.CharField(max_length=15,null=True,blank=True)
+
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['username']
     
