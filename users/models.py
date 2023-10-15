0c1d98e9a771f4c1f4c572696ac7fd54511aad4b
Kedar59 <kedarshidhaye2003@gmail.com>
2023-10-15 14:42:22 +0530

    added Skills model and ran migrations

--- users/models.py
+++ users/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.name
+
 class CustomUser(AbstractUser):
     email=models.EmailField(unique=True)
     userBio=models.CharField(max_length=255,blank=False)
     prof_path=models.CharField(max_length=1024,null=True,blank=True)
     has_prof_pic=models.BooleanField(default=False)
     prof_extension=models.CharField(max_length=15,null=True,blank=True)
+    skills = models.ManyToManyField(Skill, blank=True)
 
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['username']
-    
+
