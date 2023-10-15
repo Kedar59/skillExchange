4e3c50d2a5bcf1e7e814734a53a1be9b9a2eb53c
Kedar59 <kedarshidhaye2003@gmail.com>
2023-10-15 15:12:00 +0530

    added 5 skills to the skills table

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Skill
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
+admin.site.register(Skill)
+admin.site.register(User)
 # Register your models here.
