1532f0c70b0b96b791aa6867b15eecfc87721370
Kedar59 <kedarshidhaye2003@gmail.com>
2023-10-16 20:54:57 +0530

    user skill credential links migration commit

--- users/models.py
+++ users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    
     def __str__(self):
         return self.name
 
@@ -18,3 +18,11 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['username']
 
+User = get_user_model()
+class Credential(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    links = models.JSONField(default=list)  # A list of JSON objects
+
+    def __str__(self):
+        return f"{self.user.username}'s {self.skill.name} Credential"
